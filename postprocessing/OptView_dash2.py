db87342e81a3edf75b4c7fdff846c2a492130545
ahonzik <ahonzik@umich.edu>
2020-07-17 15:04:17 -0400

    Add features including dashed bounds

--- postprocessing/OptView_dash2.py
+++ postprocessing/OptView_dash2.py
@@ -8,7 +8,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
-from plotly import tools
+from plotly import subplots
 import numpy as np
 import argparse
 from sqlitedict import SqliteDict
@@ -44,10 +44,10 @@ funcNames = conNames + dvNames
 numIterations = hist.getCallCounters()
 objNames = hist.getObjNames()
 
-print(conNames)
-print('dvNames', dvNames)
-print(funcNames)
-print(objNames)
+# print(conNames)
+# print('dvNames', dvNames)
+# print(funcNames)
+# print(objNames)
 
 dvName = "xvar_1"
 index = dvName.split("_")[-1]
@@ -55,11 +55,32 @@ varname = dvName[::-1].replace(index + "_", "", 1)[::-1]
 # print(varname)
 
 varData = hist.getValues(names='xvars', major=False)
-print(range(len(varData['xvars'])))
-print(numIterations)
+# print(range(len(varData['xvars'])))
+# print(numIterations)
 # print(values)
 
+varData2 = hist.getValues(names='con', major=False)
+
+y=[data.real[int(0)] for data in varData['xvars']]
+# print('ok',y)
+y=[data[int(0)] for data in varData['xvars']]
+# for data in varData['xvars']:
+#     print('here',data)
+#     print(type(data[0]))
+#     print('data',data[0])
+# print(y)
+
 #Need info for each variable 
+#PYTHON2vs3 in using range
+y=[data.real[int(0)] for data in varData2['con']]
+y=[data[int(0)] for data in varData2['con']]
+
+
+dvInfo = hist.getDVInfo('xvars')
+lowerB = dvInfo['lower']
+upperB = dvInfo['upper']
+print(lowerB)
+print('dvInfo', dvInfo)
 
 
 
@@ -71,9 +92,15 @@ app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div(
     children = [
-        html.H1('OptView'),
-        html.Div(
-            [
+        html.H3('OptView'),
+        html.Div([
+                    dcc.Graph(id="plot"),
+                 ],
+                  style={"width": "80%", "margin":"0 auto"},
+        ),
+         html.Div( [
+        # html.Div(
+        #     [
                 html.Div(
                         [
                             html.H5("Design Groups"),
@@ -84,7 +111,7 @@ app.layout = html.Div(
                                 multi=True,
                             )
                         ],
-                        style={"width": "50%",  "marginRight": "1%"},
+                        style={"marginRight": "2%"},
                 ),
                 html.Div(
                         [
@@ -96,38 +123,69 @@ app.layout = html.Div(
                                 multi=True,
                             )
                         ],
-                        style={"width": "50%",  "marginLeft": "1%"},
-                )
+                        style={"marginRight": "2%"},
+                ),
+        #     ],
+        #        style={"display": "flex", "paddingLeft": "3%", "paddingRight": "3%", "width": "80%" },
+        # ),
+        # html.Div(
+        #     [
+        #         html.Div(
+        #             [
+        #                 dcc.Dropdown(
+        #                 id='dvar-child',
+        #                 # options=[{'label': i, 'value': i} for i in dvNames],
+        #                 placeholder="...then select design variable(s)",
+        #                 multi=True,
+        #                 )
+        #             ],
+        #             style={"marginRight": "1%", "paddingTop": "1%"},
+        #         ),
+        #         html.Div(
+        #             [
+        #                 dcc.Dropdown(id='func-child',
+        #                 # options=[{'label': i, 'value': i} for i in conNames],
+        #                 placeholder="...then select function variable(s)",
+        #                 multi=True,
+        #                 )
+        #             ],
+        #             style={"marginLeft": "1%", "paddingTop": "1%"},
+        #         )
+        #     ],
+        #     style={"display": "flex", "paddingLeft": "3%", "paddingRight": "3%", "width": "80%" },
+        # ),
+        html.Div(
+            [
+                html.H5("Graph Type"),
+                dcc.RadioItems(
+                    id='plot-type',
+                    options=[
+                        {'label': 'Stacked', 'value': 'stacked'},
+                        {'label': 'Shared', 'value': 'shared'},
+                    ],
+                    value='shared'
+                    )
             ],
-                style={"display": "flex", "paddingLeft": "3%", "paddingRight": "3%" },
+             style={"width": "30x", "marginRight": "2%"},
         ),
         html.Div(
             [
-                html.Div(
-                    [
-                        dcc.Dropdown(
-                        id='dvar-child',
-                        # options=[{'label': i, 'value': i} for i in dvNames],
-                        placeholder="...then select design variable(s)",
-                        multi=True,
-                        )
+                html.H5("Data Type"),
+                dcc.Checklist(
+                    id='data-type',
+                    options=[
+                        {'label': 'Show Bounds', 'value': 'bounds'},
+                        {'label': 'Show Major Iterations', 'value': 'major'},
+                        {'label': 'Log Scale', 'value': 'log'},
+                        {'label': 'Apply Scaling Factor', 'value': 'scale'},
                     ],
-                    style={"width": "50%", "marginRight": "1%", "paddingTop": "1%"},
-                ),
-                html.Div(
-                    [
-                        dcc.Dropdown(id='func-child',
-                        # options=[{'label': i, 'value': i} for i in conNames],
-                        placeholder="...then select function variable(s)",
-                        multi=True,
-                        )
-                    ],
-                    style={"width": "50%", "marginLeft": "1%", "paddingTop": "1%"},
-                )
+                    )
             ],
-            style={"display": "flex", "paddingLeft": "3%", "paddingRight": "3%" },
-        ),
-        dcc.Graph(id="plot"),
+             style={"width": "30x"},
+        ), 
+         ],
+          style={"display": "flex", "justify-content": "center", "margin":"50px"},
+         ),
     ]
 )
 
@@ -138,6 +196,8 @@ app.layout = html.Div(
     [
         dash.dependencies.Input("dvarGroup", "value"),
         dash.dependencies.Input("funcGroup", "value"),
+        dash.dependencies.Input("plot-type", "value"),
+        dash.dependencies.Input("data-type", "value"),
         # dash.dependencies.Input("dvar-child", "value"),
         # dash.dependencies.Input("func-child", "value"),
         # dash.dependencies.Input("axis_scale", "value"),
@@ -145,78 +205,162 @@ app.layout = html.Div(
         # dash.dependencies.Input("hidden-div", "value"),
     ],
 )
-def update_plot(dvarGroup, funcGroup):
+def update_plot(dvarGroup, funcGroup, plotType, dataType):
     trace = []
+    numV = 0
+    dvData = []
+    fig = {}
+    fig["layout"] = {}
+
+    numRows = 0;
+    if not dvarGroup and not funcGroup:
+        numRows = 1;
+    if funcGroup:
+        numRows += len(funcGroup)
+    if dvarGroup:
+        numRows += len(dvarGroup)
+    
+    if plotType == "stacked":
+        print('hi', numRows)
+        fig = subplots.make_subplots(rows=numRows)
+
     if dvarGroup:
         # Add all traces for each design variable's data
         for var in dvarGroup:
             # Retrieve values for specific DV 
-            varData = hist.getValues(names=var, major=False)
-            # Add each trace from the current var
-            for i in range(len(varData[var][0])):
+            numV += 1
+            dvData = []
+            if (dataType and ('major' in dataType)):
+                dvData = hist.getValues(names=var, major=True)
+            else :
+                 dvData = hist.getValues(names=var, major=False)
+            # Add each trace from the current dvar
+            for i in range(len(dvData[var][0])):
                 trace.append(
                     go.Scatter(
 #PYTHON2vs3 in using range
-                        x=list(range(len(varData[var]))),
-                        y=[data[int(i)] for data in varData[var]],
-                        name=var,
+                        x=list(range(len(dvData[var]))),
+                        y=[data.real[int(i)] for data in dvData[var]],
+                        name=var+"_"+str(i),
+                        # marker_color='blue',
                         mode="lines+markers",
                     )
                 )
-            i += 1
+                if plotType == "stacked":
+                    fig.append_trace(trace[len(trace)-1], numV, 1)
+            # add bounds information for dv variable 
+            if dataType and 'bounds' in dataType:
+                dvInfo = hist.getDVInfo(var)
+                lowerB = dvInfo['lower']
+                upperB = dvInfo['upper']
+                trace.append(
+                    go.Scatter(
+#PYTHON2vs3 in using range
+                        x=list(range(len(dvData[var]))),
+                        y=lowerB,
+                        name=var+"_lowerBound",
+                        # marker_color='blue',
+                        line = { "dash":"dash"}
+                    )
+                )
+                trace.append(
+                    go.Scatter(
+#PYTHON2vs3 in using range
+                        x=list(range(len(dvData[var]))),
+                        y=upperB,
+                        name=var+"_upperBound",
+                        # marker_color='blue',
+                        line = { "dash":"dash"}
+                    )
+                )
+                if plotType == "stacked":
+                    fig.append_trace(trace[len(trace)-1], numV, 1)
+                    fig.append_trace(trace[len(trace)-2], numV, 1)
+
+                    
+        
+    
+    funcData=[]
+    if funcGroup:
+        for var in funcGroup:
+            # Retrieve values for specific function 
+            numV += 1
+            funcData = []
+            if (dataType and ('major' in dataType)):
+                funcData = hist.getValues(names=var, major=True)
+            else :
+                 funcData = hist.getValues(names=var, major=False)
+            # Add each trace from the current fucntion
+            for i in range(len(funcData[var][0])):
+                trace.append(
+                    go.Scatter(
+#PYTHON2vs3 in using range
+                        x=list(range(len(funcData[var]))),
+                        y=[data.real[int(i)] for data in funcData[var]],
+                        name=var+"_"+str(i),
+                        # marker_color=i,
+                        mode="lines+markers",
+                    )
+                )
+                if plotType == "stacked":
+                    fig.append_trace(trace[len(trace)-1], numV, 1)
             
-    # trace = []
-    # i = 0
-    # if dvarGroup:
-    #     varData = hist.getValues(names=dvarGroup, major=False)
-    #     for var in dvar:
-    #         index = var.split("_")[-1]
-    #         varGroup = var.split("_")[0]
-    #         trace.append(
-    #             go.Scatter(
-    #                 x=range(numIterations),
-    #                 y=[data[int(index)] for data in varData[varGroup]],
-    #                 name=var,
-    #                 mode="lines+markers",
-    #             )
-    #         )
-    #         i += 1
+             # add bounds information for dv variable 
+            if dataType and 'bounds' in dataType:
+                funcInfo = hist.getConInfo(var)
+                lowerB = funcInfo['lower']
+                upperB = funcInfo['upper']
+                trace.append(
+                    go.Scatter(
+#PYTHON2vs3 in using range
+                        x=list(range(len(funcData[var]))),
+                        y=lowerB,
+                        name=var+"_lowerBound",
+                        # marker_color='blue',
+                        line = { "dash":"dash"}
+                    )
+                )
+                trace.append(
+                    go.Scatter(
+#PYTHON2vs3 in using range
+                        x=list(range(len(funcData[var]))),
+                        y=upperB,
+                        name=var+"_upperBound",
+                        # marker_color='blue',
+                        # mode="lines",
+                        line = { "dash":"dash"}
+                    )
+                )
+                if plotType == "stacked":
+                    fig.append_trace(trace[len(trace)-1], numV, 1)
+                    fig.append_trace(trace[len(trace)-2], numV, 1)
+           
 
-    # if func:
-    #     for var in func:
-    #         index = var.split("_")[-1]
-    #         varname = var[::-1].replace(index + "_", "", 1)[::-1]
-    #         trace.append(
-    #             go.Scatter(
-    #                 x=range(Opt.num_iter),
-    #                 y=[data[int(index)] for data in Opt.func_data_all[varname]],
-    #                 name=var,
-    #                 mode="lines+markers",
-    #             )
-    #         )
-    #         i += 1
 
-    fig = {}
-    fig["layout"] = {}
-    if dvarGroup or funcGroup:
-        # if type == "multi":
-        #     fig = tools.make_subplots(rows=i, cols=1)
-        #     for k in range(i):
-        #         fig.append_trace(trace[k], k + 1, 1)
-        # else:
+    # fig = {}
+    # fig["layout"] = {}
+    # if dvarGroup or funcGroup:
+    #     if plotType == "stacked":
+    #         fig = subplots.make_subplots(rows=2)
+    #         print("hi", len(trace))
+        #     for k in range(len(trace)):
+        #         fig.append_trace(trace[k], 1, 1)
+    if plotType == "shared":
         fig["data"] = trace
 
     fig["layout"].update(
+        title = {
+            "text": "OptView"
+        },
         xaxis={
             "title": {
                 "text": "Iterations",
                 "font": {"family": "Courier New, monospace", "size": 24, "color": "#7f7f7f"},
             },
-            # "type": axisscale,
         },
         yaxis={
             "title": {"text": "Data", "font": {"family": "Courier New, monospace", "size": 24, "color": "#7f7f7f"}},
-            # "type": axisscale,
+            "type": 'log' if (dataType and ('log' in dataType)) else 'linear'
         },
         height=900,
         showlegend=True,
